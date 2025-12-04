ff1a6822dddfb989f8eb99f86b7c0bb1c946116c
Carolina Pereira <a88911967@gmail.com>
2025-12-04 15:52:16 +0000

    file

---
+++ Avançado/Ficheiro_2.cpp
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<locale.h>
+
+int main(){
+
+	setlocale (LC_ALL, "Portuguese");
+	
+	FILE * fich;
+	
+	fich = fopen("texto.txt", "w");
+	
+	fprintf(fich, "%d/%d/%d", 2, 5, 2006);
+	
+	if (fich == NULL) {
+        printf("Erro: O ficheiro 'texto.txt' nao foi encontrado.\n");
+    return 1; 
+    }
+    
+    printf("Ficheiro 'texto.txt' aberto com sucesso. \n");
+    
+    fclose(fich);
+    printf("Ficheiro fechado com sucesso.\n");
+
+	return 0;
+}
