8577e19c3270b54f598f8e75c2172acd5949947b
Carolina Pereira <a88911967@gmail.com>
2026-01-07 17:13:50 +0000

    av

---
+++ Avançado/Avaliação_1.cpp
@@ -0,0 +1,5 @@
+
+ Pergunta: Indique o valor de “a” que será impresso no ecrã. Justifique a sua resposta:
+ 	
+ Resposta: O valor de "a" é 17,00 uma vez que o o valor "a" esta atrubuido ao apontador "ptr" e este
+ está apontado como uma variável do tipo double.
